a55cd66adfa172cfb15cb471bf17e2e10011dea4
lunardreamgfx <ltrkarma@gmail.com>
2022-05-01 20:06:48 +0700

    Mantap tabel

--- resources/views/listTable.blade.php
+++ resources/views/listTable.blade.php
@@ -8,26 +8,41 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Suhu Air</th>
-                                <th>Kadar pH Air</th>
-                                <th>Ketinggian Air</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($tables as $key => $value)
+
+
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
-                                    <td> {{ $value->id }} </td>
-                                    <td> {{ $value->temp_1 }} </td>
-                                    <td> {{ $value->ph_1 }} </td>
-                                    <td> {{ $value->distance_water }} </td>
+                                    <th scope="col" class="px-6 py-3">
+                                       #
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Suhu Kolam
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Kadar pH Kolam
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Ketinggian Air
+                                    </th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($tables as $key => $value)
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <td class="px-6 py-4"> {{ $value->id }} </td>
+                                        <td class="px-6 py-4"> {{ $value->temp_1 }} </td>
+                                        <td class="px-6 py-4"> {{ $value->ph_1 }} </td>
+                                        <td class="px-6 py-4"> {{ $value->distance_water }} </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+
                 </div>
             </div>
         </div>
